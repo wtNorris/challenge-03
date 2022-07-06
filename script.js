84c1d9b53f47ed68b21553c63cd2263d0548e9b3
wtNorris <106634123+wtNorris@users.noreply.github.com>
2022-07-06 09:01:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment code here
+var password = document.getElementById("password");
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordLength = 10;
+var password = ("");
 
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password +- chars.substring(randomNumber, randomNumber +1);
+}
+
+document.getElementById("password").value = password;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
